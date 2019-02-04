2117f4f88dcd976ae2d9ab9a1b7a10b9369f55a7
postolae <47318431+postolae@users.noreply.github.com>
2019-02-04 10:39:40 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    int x = 10;
+    int y = 20;
+    if (y > x)
+      System.out.println(y+" is bigger than "+x);
+  }
+}
